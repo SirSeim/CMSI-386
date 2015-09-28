7c343a01c3f32d409a0a49484a0e65eed7f1b97a
adrianlu95 <alu5@lion.lmu.edu>
2015-09-27 23:52:46 -0700

    whatever

--- lu_hw2.ml
+++ lu_hw2.ml
@@ -2,7 +2,7 @@
 
    UID: 977089943
 
-   Others With Whom I Discussed Things: Edward Seim, Joshua Kuroda, 
+   Others With Whom I Discussed Things: Edward Seim, Joshua Kuroda, Lauren Konchan, 
 
    Other Resources I Consulted:
    
@@ -45,10 +45,11 @@ let posDouble x =
 let doubleAllPos (l: int list) : int list =
     map posDouble l
 
-let _ = assert (doubleAllPos [1;2;-1;4;-3;0] = [2;4;-1;8;-3;0]);;
+let _ = assert (doubleAllPos [1; 2; -1; 4; -3; 0] = [2; 4; -1; 8; -3; 0]);;
 let _ = assert (doubleAllPos [] = [])
-let _ = assert (doubleAllPos [-1] = [-1])
-let _ = assert (doubleAllPos [-1;2] = [-1;4])
+let _ = assert (doubleAllPos [-69] = [-69])
+let _ = assert (doubleAllPos [-1; 210] = [-1; 420])
+let _ = assert (doubleAllPos [3; -9] = [6; -9])
 
 (* Problem 1b.
    A function that takes a list of pairs and returns a pair of lists.
@@ -65,7 +66,7 @@ let unzip (l: ('a * 'b) list) : 'a list * 'b list  =
 
 
 let _ = assert (unzip [] = ([], []))
-let _ = assert (unzip [(1, 'a')] = ([0], ['a']))
+let _ = assert (unzip [(0, 'a')] = ([0], ['a']))
 let _ = assert (unzip [(1,'a'); (2, 'b')] = ([1; 2], ['a'; 'b']));;
 
 (* Problem 1c.
@@ -85,13 +86,13 @@ let _ = assert (encode ['a';'a';'a';'b';'c';'c'] = [(3,'a');(1,'b');(2,'c')]);; 
 let multiplicate n x = (n * 10) + x
 
 let intOfDigits (l : int list) : int = 
-    fold_left (multiplicate n x) 0 l
+    fold_left (multiplicate) 0 l
 
 let _ = assert (intOfDigits [] = 0)
-let _ = assert (intOfDigits [0] = 0)
-let _ = assert (intOfDigits [5] = 5)
-let _ = assert (intOfDigits [5;4] = 54)
-let _ = assert (intOfDigits [3;4;5] = 345)
+let _ = assert (intOfDigits [0; 0; 0; 0] = 0)
+let _ = assert (intOfDigits [1] = 1)
+let _ = assert (intOfDigits [1; 0; 0; 0; 1] = 10001)
+let _ = assert (intOfDigits [0; 0; 0; 1] = 1)
 
 (***********************************************************************
  * Problem 2: Defining higher-order functions.
@@ -108,10 +109,14 @@ let _ = assert (intOfDigits [3;4;5] = 345)
    Do not use any functions from the List module or other modules.
  *)
 
-(* let rec map2 (funk : ('a -> 'b -> 'c)) (l1 : 'a list) (l2 : 'b list) : 'c list =
+let rec map2 (f : ('a -> 'b -> 'c)) (l1 : 'a list) (l2 : 'b list) : 'c list = 
+    match l1, l2 with
+    | [], [] -> []
+    | [], _ -> []
+    | _, [] -> []
+    | hd1::tl1, hd2::tl2 -> (f hd1 hd2)::(map2 f tl1 tl2)
 
-
-let _ = assert (map2 (fun x y -> x*y) [1;2;3] [4;5;6] = [1*4; 2*5; 3*6]);; *)
+let _ = assert (map2 (fun x y -> x*y) [1; 2; 3] [4; 5; 6] = [1*4; 2*5; 3*6]);;
 
 (* Problem 2b.
 
@@ -122,11 +127,14 @@ let _ = assert (map2 (fun x y -> x*y) [1;2;3] [4;5;6] = [1*4; 2*5; 3*6]);; *)
 
    Implement zip as a function whose entire body is a single call to
    map2.
- *)
+*)
 
-(* let zip : 'a list -> 'b list -> ('a * 'b) list = map2 TODO
+let helpingHand x y = (x, y)
 
-let _ = assert (zip [1;2] ['a';'b']  = [(1,'a');(2,'b')]);; *)
+let zip (l1 : 'a list) (l2 : 'b list) : ('a * 'b) list = 
+    map2 helpingHand l1 l2
+
+let _ = assert (zip [1;2] ['a';'b']  = [(1,'a');(2,'b')]);;
 
 (* Problem 2c.
 
@@ -190,11 +198,28 @@ let _ = assert (foldn (fun x y -> x-y) 5 1 = 5 - (4 - (3 - (2 - 1))));;*)
    simply add new entries to the front of the list, and accordingly
    get1 should return the leftmost value whose associated key matches
    the given key.
+*)  
 
-   empty1: unit -> ('a * 'b) list
-   put1: 'a -> 'b -> ('a * 'b) list -> ('a * 'b) list
-   get1: 'a -> ('a * 'b) list -> 'b option
- *)  
+
+let empty1 (a : unit) : ('a * 'b) list = []
+
+let put1 (key : 'a) (value : 'b) (dict : ('a * 'b) list) : ('a * 'b) list =
+   (key, value)::dict
+
+let rec get1 (key : 'a) (dict : ('a * 'b) list) : 'b option = 
+   match dict with
+   | [] -> None
+   | (k, v)::dicttl -> if (k = key) then
+                            Some v
+                        else get1 key dicttl
+
+let _ = assert (empty1 () = [])
+let dictionary = put1 "test" 1 (empty1 ())
+let _ = assert (get1 "test" dictionary = Some 1)
+let dictionary = put1 "test2" 2 dictionary
+let _ = assert (get1 "test" dictionary = Some 1)
+let _ = assert (get1 "test2" dictionary = Some 2)
+let _ = assert (get1 "fail" dictionary = None)
 
 (* Problem 3b.
 
@@ -215,9 +240,27 @@ let _ = assert (foldn (fun x y -> x-y) 5 1 = 5 - (4 - (3 - (2 - 1))));;*)
    put2: 'a -> 'b -> ('a,'b) dict2 -> ('a,'b) dict2
    get2: 'a -> ('a,'b) dict2 -> 'b option
  *)  
-    
-(* type ('a,'b) dict2 = Empty | Entry of 'a * 'b * ('a,'b) dict2 *)
-    
+type ('a,'b) dict2 = Empty | Entry of 'a * 'b * ('a,'b) dict2
+
+let empty2 (u : unit) : ('a,'b) dict2 = Empty
+
+let put2 (key : 'a) (value : 'b) (dict : ('a,'b) dict2) : ('a,'b) dict2 = 
+    Entry (key, value, dict)
+
+let rec get2 (key : 'a) (dict : ('a,'b) dict2) : 'b option = 
+    match dict with
+    | Empty -> None
+    | Entry (k, v, dicttl) when k = key -> Some v
+    | Entry (k, v, dicttl) -> get2 key dicttl
+
+let _ = assert (empty2 () = Empty)
+let dictionary = put2 "test" 1 (empty2 ())
+let _ = assert (get2 "test" dictionary = Some 1)
+let dictionary = put2 "test2" 2 dictionary
+let _ = assert (get2 "test" dictionary = Some 1)
+let _ = assert (get2 "test2" dictionary = Some 2)
+let _ = assert (get2 "fail" dictionary = None)
+
 (* Problem 3c
 
    Conceptually, a dictionary is just a function from keys to values.
@@ -252,6 +295,25 @@ let _ = assert (foldn (fun x y -> x-y) 5 1 = 5 - (4 - (3 - (2 - 1))));;*)
    empty3: unit -> ('a,'b) dict3
    put3: 'a -> 'b -> ('a,'b) dict3 -> ('a,'b) dict3
    get3: 'a -> ('a,'b) dict3 -> 'b option
- *)  
+ *)
 
-(* type ('a,'b) dict3 = ('a -> 'b option)*)
+type ('a,'b) dict3 = ('a -> 'b option)
+let none n = None
+
+let empty3 (a : unit) : ('a,'b) dict3 = (none)
+
+let put3 (k : 'a) (value : 'b) (dict : ('a,'b) dict3) : ('a,'b) dict3 =
+    (fun key -> if (key = k) then
+                    Some value
+                else dict key)
+
+let get3 (k : 'a) (dict : ('a,'b) dict3) : 'b option =
+    dict k
+
+let _ = assert (empty3 () 0 = None)
+let dictionary = put3 "test" 1 (empty3 ())
+let _ = assert (get3 "test" dictionary = Some 1)
+let dictionary = put3 "test2" 2 dictionary
+let _ = assert (get3 "test" dictionary = Some 1)
+let _ = assert (get3 "test2" dictionary = Some 2)
+let _ = assert (get3 "fail" dictionary = None)
